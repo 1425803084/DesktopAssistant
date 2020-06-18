652e55979deae16301895ee9a64ca2bb9a3938ef
huahuafafa <1426057759@qq.com>
2020-06-19 00:07:17 +0800

    frame

--- app/frame/Form2.Designer.cs
+++ app/frame/Form2.Designer.cs
@@ -185,9 +185,9 @@
             this.label2.ForeColor = System.Drawing.Color.MediumOrchid;
             this.label2.Location = new System.Drawing.Point(33, 428);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.Size = new System.Drawing.Size(84, 20);
             this.label2.TabIndex = 11;
-            this.label2.Text = "Tag";
+            this.label2.Text = "Tag分类";
             // 
             // label1
             // 
