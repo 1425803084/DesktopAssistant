2da5ff44b2e88e2a6f0427ed33aeb48313cec336
焯希 庞 <1425803084@qq.com>
2020-06-19 06:28:16 +0800

    taskDetail

---
+++ app/TaskDetail/taskdetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopAssistant.app.TaskDetail
+{
+    public partial class taskdetail : UserControl
+    {
+        public taskdetail()
+        {
+            InitializeComponent();
+        }
+
+        private void phlTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            MessageBox.Show("111");
+            ProcessRatio.Text = phlTrackBar1.T_Value.ToString();
+        }
+
+    }
+}
